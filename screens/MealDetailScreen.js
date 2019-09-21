073b825a4d174b3de27448079154f20e139f7a07
Dinesh <dineshthiru.1998@gmail.com>
2019-09-21 08:31:01 +0530

    commit for MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -3,6 +3,10 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import  HeaderButton from '../components/HeaderButton';
+import { Header } from 'react-navigation-stack';
+
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
 
@@ -21,8 +25,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return  {
-    headerTitle: selectedMeal.title
-  }; 
+    headerTitle: selectedMeal.title,
+    headerRight: (
+    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      <Item
+       title='Favorite' 
+       iconName='ios-star' 
+       onPress={() => {
+         console.log('Mark as favorite!');
+       }}/>
+    </HeaderButtons>
+    )};
 };
 
 const styles = StyleSheet.create({
